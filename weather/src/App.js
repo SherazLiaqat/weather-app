a0f7298462463a16891ec09e05cc23f912786f9a
SherazLiaqat <sherazliaqat099@gmailcom>
2021-12-07 18:52:35 +0500

    card

---
+++ weather/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Searchweather from './Components/Searchweather';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>weather app</h1>
+     <Searchweather/>
+    </div>
+  );
+}
+
+export default App;
